d168fdaa727343b143e4769a06e1c1aa3cfbdf76
SaKKo <saklism@gmail.com>
2020-11-05 11:52:56 +0700

    cherry pick components

--- plugins/iview.js
+++ plugins/iview.js
@@ -1,6 +1,25 @@
 import Vue from 'vue';
-import ViewUI from 'view-design';
+import {
+  Button,
+  ButtonGroup,
+  Icon,
+  Layout,
+  Content,
+  Footer,
+  Modal,
+  Divider,
+} from 'view-design';
+
+Vue.component('Button', Button);
+Vue.component('Icon', Icon);
+Vue.component('Layout', Layout);
+Vue.component('Content', Content);
+Vue.component('Footer', Footer);
+Vue.component('Modal', Modal);
+Vue.component('Divider', Divider);
+Vue.component('ButtonGroup', ButtonGroup);
+
 // import VueYoutube from 'vue-youtube';
 
-Vue.use(ViewUI);
+// Vue.use(ViewUI);
 // Vue.use(VueYoutube);
